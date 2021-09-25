43037d704e193d39a7c432af50938d60be5e088f
rushya <rushikeshdabade8@gmail.com>
2021-09-25 13:44:44 +0530

    Added: Content Of The Program

---
+++ src/empolyeewage/Empwageuc1.java
@@ -0,0 +1,13 @@
+package empolyeewage;
+
+public class Empwageuc1 {
+	public static void main(String[] args) {
+		int is_FULL=1;
+		double empCheck= Math.floor(Math.random()*10 %2);
+		if(empCheck==is_FULL)
+			System.out.println("Empolyee is present");
+		else
+			System.out.println("Empolyee is absent");
+	}
+
+}
